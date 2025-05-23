cb30dbb032e2f615ad605b6daa44d22dba6abde7
levygoat <taheemmanuel58@gmail.com>
2025-05-23 13:20:52 +0000

    Add files via upload

---
+++ Tahe/exam/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import dashboard
+
+urlpatterns = [
+    path('dashboard/', dashboard, name='dashboard'),
+
+    # Vues d'authentification intégrées
+    path('login/',
+         auth_views.LoginView.as_view(
+             template_name='users/login.html',
+             redirect_authenticated_user=True),
+         name='login'),
+
+    path('logout/',
+         auth_views.LogoutView.as_view(
+             next_page='home'),
+         name='logout'),
+]
